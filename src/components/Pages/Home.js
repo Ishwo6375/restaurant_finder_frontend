58926499b8443d494e570492d42684cf30b7d282
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-08-12 20:41:07 -0400

    added paragarph and background image to homepage

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -8,10 +8,11 @@ function Home() {
         <div>
          <div className="homepage-items">
             <h1 className="home-head">Welcome to Restaurant Finder.com</h1>
+             <p className="home-hp">Find your desired Restaurant...</p>
             <p className="home-p">New At Restaurant Business???</p>
             <p className="home-p">Add your Restaurant to Our App</p>
             <p className="home-p">Click Add Now to add your Restaurant</p>
-            <Link className="btn btn-secondary" to={"/add"}>Add Now!!</Link>
+            <Link className="btn btn-secondary home-link" to={"/add"}>Add Now!!!</Link>
          </div>
         </div>
         </div>
